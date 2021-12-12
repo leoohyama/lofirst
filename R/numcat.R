3fb72073ecfe57ab9f508c3cf1659e19f7de3b5d
leoohyama <leo.ohyama@ufl.edu>
2021-12-12 17:00:09 -0500

    Initial code for functions

---
+++ R/numcat.R
@@ -0,0 +1,61 @@
+#new function idea, making a numeric into categorical factors! Binning data
+
+df<-datasets::iris
+
+numcat<-function(data , num, cat_name, cat_no, labels){
+  if(is.na(labels)){
+    #if labels are NA use generic labels
+
+
+    #range of numbers divided by specified number of categories
+    interval<-(max(data[[num]], na.rm = T)-min(data[[num]], na.rm = T))/cat_no
+
+    #make a list of generic labels for length of category number
+    label_cat<-LETTERS[seq( from = 1, to = cat_no)]
+
+
+    #empty vector to fill in for intervals
+    interval_slots<-vector()
+
+    for (i in 1:length(label_cat)){
+      interval_slots[i]<-interval*i
+    }
+
+    combinations<-cbind(interval_slots, label_cat)
+
+    #new column for categories
+    data<- dplyr::mutate(data, cat_name = NA)
+
+    #Just a for loop at this point?
+    for(i in 1:nrow(combinations)){
+
+      if(i == 1){
+      #if the first level
+        data$cat_name[data[[num]] < combinations[i,1]]<-combinations[i,2]
+      }
+      else if(i == tail(1:nrow(combinations), n =1)){
+        #if last number
+        data$cat_name[data[[num]] > combinations[i-1,1]]<-combinations[i,2]
+
+
+      }
+      else{
+        #inbetween values
+        data$cat_name[data[[num]] > combinations[i-1,1] &
+                        data[[num]] < combinations[i+1,1]]<-combinations[i,2]
+
+
+      }
+
+
+    }
+
+  }else{
+    print("not done yet")
+  }
+  return(data)
+}
+numcat(data = df, num = "Petal.Length",
+       cat_name = "NewCategories",
+       cat_no = 5,
+       labels = NA)
---
+++ R/visualize_traits.R
@@ -0,0 +1,37 @@
+#First function, plot species traits
+visualize_traits<-function(data, trait1, trait2, taxacol){
+  if(is.na(trait1)){
+    print("Need an input for at least one trait!")
+  }else{
+    if(is.na(trait2)){
+      if(trait1 %in% colnames(data) &
+         taxacol %in% colnames(data)){
+        #passed test plot single trait by species
+        ggplot2::ggplot(data = data) +
+          ggplot2::geom_boxplot(ggplot2::aes(x = .data[[taxacol]],
+                                             y = .data[[trait1]]))
+      }else{
+        print("trait1 or taxa does not match the column names of the data")
+      }
+    }else{
+      #check  both trait 1 and trait 2 for valid names
+      if(trait1 %in% colnames(data) &
+         trait2 %in% colnames(data) &
+         taxacol %in% colnames(data)){
+        #passed test plot two traits by species
+
+        ggplot2::ggplot(data = data) +
+          ggplot2::geom_point(ggplot2::aes(x = .data[[trait1]],
+                                             y = .data[[trait2]],
+                                           color = .data[[taxacol]])
+                                )
+
+      }else{
+        print("trait1 or trait 2 or taxa does not match the column names of the data")
+      }
+    }
+  }
+}
+
+
+
