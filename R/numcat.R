ffe2753bf6275183076add070663b6080e599961
leoohyama <leo.ohyama@ufl.edu>
2021-12-12 20:59:17 -0500

    Initial commit

--- R/numcat.R
+++ R/numcat.R
@@ -6,13 +6,11 @@ numcat<-function(data , num, cat_name, cat_no, labels){
   if(is.na(labels)){
     #if labels are NA use generic labels
 
-
     #range of numbers divided by specified number of categories
     interval<-(max(data[[num]], na.rm = T)-min(data[[num]], na.rm = T))/cat_no
 
     #make a list of generic labels for length of category number
     label_cat<-LETTERS[seq( from = 1, to = cat_no)]
-
 
     #empty vector to fill in for intervals
     interval_slots<-vector()
@@ -44,18 +42,21 @@ numcat<-function(data , num, cat_name, cat_no, labels){
         data$cat_name[data[[num]] > combinations[i-1,1] &
                         data[[num]] < combinations[i+1,1]]<-combinations[i,2]
 
-
       }
 
-
     }
+    return(data)
 
-  }else{
+  }else if(length(labels) == cat_no){
+
     print("not done yet")
+  }else{
+    print("Number of categories and number of labels are not the same!")
   }
-  return(data)
+
 }
 numcat(data = df, num = "Petal.Length",
        cat_name = "NewCategories",
        cat_no = 5,
-       labels = NA)
+       labels = c( "c", "d", "f", "r"))
+is.na(labels)
