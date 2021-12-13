ebe7b5d2ce8a2c7c336993d1e6b5bdbc8124825c
leoohyama <leo.ohyama@ufl.edu>
2021-12-12 21:48:37 -0500

    change function name

--- R/num_to_cat.R
+++ R/num_to_cat.R
@@ -2,7 +2,7 @@
 
 df<-datasets::iris
 
-numcat<-function(data , num, cat_name, cat_no, labels){
+num_to_cat<-function(data , num, cat_name, cat_no, labels){
   if(is.na(labels)){
     #if labels are NA use generic labels
 
@@ -49,14 +49,52 @@ numcat<-function(data , num, cat_name, cat_no, labels){
 
   }else if(length(labels) == cat_no){
 
-    print("not done yet")
+    #if labels are specified
+
+    #range of numbers divided by specified number of categories
+    interval<-(max(data[[num]], na.rm = T)-min(data[[num]], na.rm = T))/cat_no
+
+    #make a list of generic labels for length of category number
+    label_cat<-labels
+
+    #empty vector to fill in for intervals
+    interval_slots<-vector()
+
+    for (i in 1:length(label_cat)){
+      interval_slots[i]<-interval*i
+    }
+
+    combinations<-cbind(interval_slots, label_cat)
+
+    #new column for categories
+    data<- dplyr::mutate(data, cat_name = NA)
+
+    #Just a for loop at this point?
+    for(i in 1:nrow(combinations)){
+
+      if(i == 1){
+        #if the first level
+        data$cat_name[data[[num]] < combinations[i,1]]<-combinations[i,2]
+      }
+      else if(i == tail(1:nrow(combinations), n =1)){
+        #if last number
+        data$cat_name[data[[num]] > combinations[i-1,1]]<-combinations[i,2]
+
+
+      }
+      else{
+        #inbetween values
+        data$cat_name[data[[num]] > combinations[i-1,1] &
+                        data[[num]] < combinations[i+1,1]]<-combinations[i,2]
+      }
+    }
+    return(data)
+
   }else{
     print("Number of categories and number of labels are not the same!")
   }
-
 }
-numcat(data = df, num = "Petal.Length",
+num_to_cat(data = df, num = "Petal.Length",
        cat_name = "NewCategories",
-       cat_no = 5,
+       cat_no = 4,
        labels = c( "c", "d", "f", "r"))
-is.na(labels)
